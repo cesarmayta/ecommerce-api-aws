5165ecb102ef6ca0b5ccf8b40069a36dfcc8a930
César Mayta <cesarmayta@gmail.com>
2024-03-27 21:45:09 -0500

    deploy 1 dev

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-d+gn^h*c@iy&1nh1b&^lyo6$jkut927l-_b^yp+a4_z#8pdsr+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "db_ecommerce_g24",
-        "USER": "postgres",
-        "PASSWORD": "root",
-        "HOST": "127.0.0.1",
-        "PORT": "5433",
+        "NAME": config('PG_DB'),
+        "USER": config('PG_USER'),
+        "PASSWORD": config('PG_PWD'),
+        "HOST": config('PG_HOST'),
+        "PORT": config('PG_PORT'),
     }
 }
 
